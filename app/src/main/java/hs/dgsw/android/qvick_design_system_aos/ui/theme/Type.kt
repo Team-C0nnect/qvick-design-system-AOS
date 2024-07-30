ff544d7d6a72039c64d3273585f97522aa16835e
hom237 <jinsic4568@gmail.com>
2024-07-30 16:36:30 +0900

    :art: 디렉토리 구조 변경 및 폰트 파일 추가

--- app/src/main/java/hs/dgsw/android/qvick_design_system_aos/ui/theme/Type.kt
+++ app/src/main/java/hs/dgsw/android/qvick_design_system_aos/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package hs.dgsw.android.qvick_design_system_aos.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import hs.dgsw.android.qvick_design_system_aos.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,12 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+val pretendard = FontFamily(
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_black, FontWeight.Black)
 )
