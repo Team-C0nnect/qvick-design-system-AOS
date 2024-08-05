acc2f6d4caa6b2ca5f3ef98d22ed8e32502a4208
hom237 <jinsic4568@gmail.com>
2024-08-06 00:03:22 +0900

    :art: 내부 세부 내용변경

--- app/src/main/java/hs/dgsw/android/qvick_design_system_aos/MainActivity.kt
+++ app/src/main/java/hs/dgsw/android/qvick_design_system_aos/MainActivity.kt
@@ -1,6 +1,7 @@
 package hs.dgsw.android.qvick_design_system_aos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,8 +9,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.SaverScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import hs.dgsw.android.qvick_design_system_aos.ui.textfield.A
+import hs.dgsw.android.qvick_design_system_aos.ui.textfield.DuckieTextField
+import hs.dgsw.android.qvick_design_system_aos.ui.textfield.TextField
 import hs.dgsw.android.qvick_design_system_aos.ui.theme.QvickdesignsystemAOSTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val test : MutableState<String> = rememberSaveable {
+                        mutableStateOf("")
+                    }
+                    DuckieTextField(
+                        text = test.value,
+                        onValueChange = {old , new, length ->
+                            test.value =  if (new.length > length || new.any { !it.isDigit() }) old else new
+                            test.value
+                        }
+                    )
                 }
             }
         }
