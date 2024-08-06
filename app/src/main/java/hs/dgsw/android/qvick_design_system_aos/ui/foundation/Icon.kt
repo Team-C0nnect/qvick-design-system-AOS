087217cb2061a658151425d69caa580ee8903f2d
hom237 <jinsic4568@gmail.com>
2024-08-06 20:37:46 +0900

    :art: icon이슈 변경, infoBar 합병

--- app/src/main/java/hs/dgsw/android/qvick_design_system_aos/ui/foundation/Icon.kt
+++ app/src/main/java/hs/dgsw/android/qvick_design_system_aos/ui/foundation/Icon.kt
@@ -1,18 +1,24 @@
 package hs.dgsw.android.qvick_design_system_aos.ui.foundation
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import hs.dgsw.android.qvick_design_system_aos.R
 import hs.dgsw.android.qvick_design_system_aos.ui.theme.common0
 
 @Composable
 fun IcAccount(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_account),
         contentDescription = contentDescription,
         tint = tint
@@ -21,10 +27,12 @@ fun IcAccount(
 
 @Composable
 fun IcEdit(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_edit),
         contentDescription = contentDescription,
         tint = tint
@@ -33,10 +41,12 @@ fun IcEdit(
 
 @Composable
 fun IcHome(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_home),
         contentDescription = contentDescription,
         tint = tint
@@ -45,10 +55,12 @@ fun IcHome(
 
 @Composable
 fun IcNavigate_before(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_navigate_before),
         contentDescription = contentDescription,
         tint = tint
@@ -57,22 +69,28 @@ fun IcNavigate_before(
 
 @Composable
 fun IcNavigate_next(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
+
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_navigate_next),
         contentDescription = contentDescription,
         tint = tint
+
     )
 }
 
 @Composable
 fun IcNone(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_none),
         contentDescription = contentDescription,
         tint = tint
@@ -81,10 +99,12 @@ fun IcNone(
 
 @Composable
 fun IcNotifications(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_notifications),
         contentDescription = contentDescription,
         tint = tint
@@ -93,10 +113,12 @@ fun IcNotifications(
 
 @Composable
 fun IcPerson(
+    modifier: Modifier = Modifier,
     contentDescription: String?,
     tint: Color = common0,
 ) {
     Icon(
+        modifier = modifier.size(32.dp),
         painter = painterResource(id = R.drawable.ic_person),
         contentDescription = contentDescription,
         tint = tint
