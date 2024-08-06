e227ebc4a15f6f707f7054c2d02704498abdd724
hom237 <jinsic4568@gmail.com>
2024-08-06 18:05:44 +0900

    :art: icon 추가

--- app/src/main/java/hs/dgsw/android/qvick_design_system_aos/ui/foundation/Icon.kt
+++ app/src/main/java/hs/dgsw/android/qvick_design_system_aos/ui/foundation/Icon.kt
@@ -8,12 +8,96 @@ import hs.dgsw.android.qvick_design_system_aos.R
 import hs.dgsw.android.qvick_design_system_aos.ui.theme.common0
 
 @Composable
+fun IcAccount(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_account),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
+fun IcEdit(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_edit),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
+fun IcHome(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_home),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
+fun IcNavigate_before(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_navigate_before),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
+fun IcNavigate_next(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_navigate_next),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
+fun IcNone(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_none),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
 fun IcNotifications(
-    contentDescription : String?,
-    tint : Color = common0
-){
+    contentDescription: String?,
+    tint: Color = common0,
+) {
     Icon(
         painter = painterResource(id = R.drawable.ic_notifications),
+        contentDescription = contentDescription,
+        tint = tint
+    )
+}
+
+@Composable
+fun IcPerson(
+    contentDescription: String?,
+    tint: Color = common0,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_person),
         contentDescription = contentDescription,
         tint = tint
     )
